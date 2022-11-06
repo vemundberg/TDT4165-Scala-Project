58ada30b87c2153b0bad3a73d0917aa83b7b5370
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2022-11-06 22:12:16 +0100

    Added showcase function for deadlock-ing

--- task2.scala
+++ task2.scala
@@ -8,7 +8,7 @@ def PrintTest(): Unit = {
   val test = FunToThread(() => println("Dette er en kul test"))
   test.start()
 }
-  
+
 private var counter: Int = 0
 def increaseCounter(): Unit = {
 counter += 1
@@ -22,7 +22,7 @@ def printCounter(): Unit = {
 // This phenomenon is called the race condition, and occurs when two or more threads acess a shared variable,
 // where the output of the program is dependent on the timing at which the separate threads got executed. We have illustrated a situation where
 // two threads are changing the state of a shared variable, and one reads it, but threre is no sync between them, meaning they execute there statements
-// at a non sequential manner, and the output is not reliable. So any situation where we read and write a shared variable, we need "atomocity". 
+// at a non sequential manner, and the output is not reliable. So any situation where we read and write a shared variable, we need "atomocity".
 
 val atomicCounter = new AtomicInteger(0)
 def increaseCounterSafe(): Unit = this.synchronized {
@@ -59,5 +59,29 @@ tester()
 
 testerSafe()
 
-// A deadlock in concurrency occurs when multiple values are being accessed, but a collision occurs, making all of them wait for eachother (or itself). 
+// Task2d
+// A deadlock in concurrency occurs when multiple values are being accessed, but a collision occurs, making all of them wait for each other (or itself).
 // This results in none of them taking any action and thus creating a deadlock.
+
+object deadLObj {
+  lazy val initialState = 42
+  lazy val start = deadLObjTwo.initialState
+}
+
+object deadLObjTwo {
+  lazy val initialState = deadLObj.initialState
+}
+
+def CheckDeadlock: Unit = {
+    val result = Future.sequence(Seq(
+      Future {
+        deadLObj.start
+      },
+      Future {
+        deadLObjTwo.initialState
+      }
+    ))
+    Await.result(result, 5.second)
+  }
+
+  CheckDeadlock
